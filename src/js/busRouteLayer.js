2189192613b04e073a14a3ca89a7d0e0a02a9dab
William Qi <hello@williamqi.com>
2017-08-25 02:22:02 -0700

    add capacity to bus routes

--- src/js/busRouteLayer.js
+++ src/js/busRouteLayer.js
@@ -20,21 +20,34 @@ function init(mapRef, busRouteData) {
   map = mapRef;
 }
 
+// Get weight for route based on capacity
+function getWeight(capacity) {
+  if (capacity > 50000) {
+    return 7;
+  } else if (capacity > 30000) {
+    return 5;
+  } else if (capacity > 10000) {
+    return 3;
+  }
+
+  return 1;
+}
+
 // Draw BusRouteLayer on map
 function addLayer(map, busRoutes) {
-  const brStyle = {
-    weight: 2,
-    fillOpacity: 0.2,
-  };
-
   const busRouteLayer = L.geoJson(busRoutes, {
-    style: brStyle,
+    style: feature => ({
+      color: '#249A48',
+      weight: getWeight(feature.properties.capacity),
+      opacity: 0.25,
+    }),
     onEachFeature: (feature, layer) => {
       const header = `${feature.properties.trips_ro_2} - ${feature.properties.trips_ro_3}`;
       const html = `<div class='mapPopup'>
                       <b class='popupHeader'> ${header} </b><br/ >
                       <br />
                       <b>Headsign:</b> ${feature.properties.trips_tr_1}<br />
+                      <b>Daily Commuter Capacity:</b> ${feature.properties.capacity}<br />
                     </div>`;
       layer.bindPopup(html);
     },
